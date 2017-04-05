be8a6433237f5e2cbbb14c6a09d816d678d923a2
Thien Tran <thientran1986@gmail.com>
2017-04-05 16:29:14 +0000

    Renaming files

--- dockerWorkflow/dockerRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
- 
-docker run -d -p 20088:20088 --name=${1} --env-file=./env ${1}
--- dockerWorkflow/dockerStop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ "$(docker ps -a --no-trunc | grep ${1} | awk '{print $1}' > /dev/null)" != "" ]]; then
-	# do something
-	docker ps -a --no-trunc | grep ${1} | awk '{print $1}' | xargs docker stop | xargs docker rm
-else
-	echo "No container to be removed."
-fi
